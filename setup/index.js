bb39e5010916546c5a9bfee5ed33f9558de5de17
Samyak Shah <samyakshah123@gmail.com>
2023-12-27 20:21:08 +0530

    Bug fix while creating model

--- setup/index.js
+++ setup/index.js
@@ -5,8 +5,11 @@ const mongoose = require('mongoose');
 const startSetup = async () => {
     await mongoose.connect(process.env.MONGODB_URI);
 
-    const models = await DomainModel.find({clientCode: process.env.BASE_CLIENT_CODE, clientId: process.env.BASE_CLIENT_ID});
-    console.log('Models found:');
+    const models = await DomainModel.find({ clientCode: process.env.BASE_CLIENT_CODE, clientId: process.env.BASE_CLIENT_ID });
+    for (var i in models) {
+        console.log(JSON.stringify(models[i]))
+    }
+    return
 
     for (var i in models) {
         const model = models[i];
